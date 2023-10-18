96cffcdb8d145788f08b791b35c1911c153cbf67
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-10-18 14:55:51 -0400

    added homepage css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "./index.css";
 const root = document.querySelector("#root");
 
 function component() {
   console.log("test");
   const element = document.createElement("div");
-  element.innerHTML = "Welcome to my portfolio";
   return element;
 }
 root.appendChild(component());
