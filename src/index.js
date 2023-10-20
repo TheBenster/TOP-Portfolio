baca56fd44e7842675b12f1e70d788b8ac08fd82
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-10-20 14:20:46 -0400

    add css for about section, add headshot.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import "./index.css";
-const root = document.querySelector("#root");
-
-function component() {
-  console.log("test");
-  const element = document.createElement("div");
-  return element;
-}
-root.appendChild(component());
