676399b4508c2d99a21aedd9747494234c0468ff
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-10-25 12:48:07 -0400

    add scroll feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./index.css";
+
+// Select the section you want to monitor the scroll for.
+const aboutPage = document.getElementById("about-page");
+
+// Define a function to perform the action.
+function doSomethingOnScroll() {
+  const scrollPosition = window.scrollY;
+  const aboutPageTop = aboutPage.getBoundingClientRect().top;
+
+  if (scrollPosition > aboutPageTop) {
+    alert("User has scrolled to the 'about' section");
+  } else {
+    alert("User hasn't scrolled to the 'about' section yet");
+  }
+}
+
+// Add an event listener to the window for the scroll event.
+window.addEventListener("scroll", doSomethingOnScroll);
