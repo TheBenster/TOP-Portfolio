75e79a5509f8bfcaa77512ead4765ac988a1c39b
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-11-13 12:09:09 -0500

    change js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import "./index.css";
 
-// Select the section you want to monitor the scroll for.
-// Get all the navigation links
 const navLinks = document.querySelectorAll(".link-to");
 
-// Function to highlight the appropriate navigation link
 function highlightNavLink() {
   const scrollPosition = window.scrollY;
   console.log("test");
 
-  // Loop through each section and check if it's in view
   navLinks.forEach((link) => {
     const targetId = link.getAttribute("href").substring(1);
     const targetSection = document.getElementById(targetId);
@@ -18,7 +14,6 @@ function highlightNavLink() {
       const sectionTop = targetSection.offsetTop;
       const sectionBottom = sectionTop + targetSection.clientHeight;
 
-      // Highlight the link if the section is in view
       if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
         link.classList.add("active");
       } else {
@@ -28,8 +23,6 @@ function highlightNavLink() {
   });
 }
 
-// Add a scroll event listener to highlight the appropriate link
 window.addEventListener("scroll", highlightNavLink);
 
-// Initial call to highlightNavLink to set the initial state
 highlightNavLink();
